4bdda5bdb2c3ec090e48dcf88de3f75e5ccdfbb2
影形 <2339254049@qq.com>
2022-03-12 18:49:21 +0800

    bool布尔类型

---
+++ 编程练习/bool布尔类型/bool布尔类型/bool布尔类型.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	bool flag = true;
+
+	cout << flag << endl;
+
+	flag = false;
+
+	cout << flag << endl;
+
+	cout << sizeof(flag) << endl;
+
+	flag = 2;
+	cout << flag << endl;
+	flag = 0;
+	cout << flag << endl;
+	flag = -9;
+	cout << flag << endl;
+
+
+	system("pause");
+
+	return 0;
+}
