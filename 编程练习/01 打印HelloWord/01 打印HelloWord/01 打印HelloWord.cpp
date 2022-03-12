d53f7c731b1adb04216b45c696ba159764eff411
影形 <2339254049@qq.com>
2022-03-12 12:39:50 +0800

    打印hello word

---
+++ 编程练习/01 打印HelloWord/01 打印HelloWord/01 打印HelloWord.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello World" << endl;
+	cout << "你好" << endl;
+
+	system("pause");
+
+	return 0;
+}
