09eff4cb4cfa174fae4230ade9341171170b6593
影形 <2339254049@qq.com>
2022-03-12 13:08:13 +0800

    注释

---
+++ 02 注释/02 注释/02 注释.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <iostream>
+using namespace std;
+
+/*
+	这是一个多行注释
+*/
+
+//这是一个单行注释
+
+int main(void)
+{
+	//屏幕打印hello 你好
+	cout << "Hello 你好" << endl;
+
+	system("pause");
+
+	return 0;
+}
