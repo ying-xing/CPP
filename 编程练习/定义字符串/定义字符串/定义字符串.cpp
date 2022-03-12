a2e3d17848cc8bc8fe9e29e45b766bd8d73b27af
影形 <2339254049@qq.com>
2022-03-12 18:39:37 +0800

    定义字符串

---
+++ 编程练习/定义字符串/定义字符串/定义字符串.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <iostream>
+using namespace std;
+
+#include <string>
+
+int main(void)
+{
+	char str1[] = "hello";
+	string str2 = "你好";
+
+	cout << str1 << endl;
+	cout << str2 << endl;
+
+	system("pause");
+
+	return 0;
+}
