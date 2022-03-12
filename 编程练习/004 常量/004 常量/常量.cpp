4db8bea9107563e740630e4975d17414de127a57
影形 <2339254049@qq.com>
2022-03-12 15:44:20 +0800

    常量

---
+++ 编程练习/004 常量/004 常量/常量.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <iostream>
+using namespace std;
+
+#define DAY 7
+
+int main(void)
+{
+	const int month = 12;
+
+	cout << "一周有" << DAY << "天" << endl;
+	cout << "一年有" << month << "月" << endl;
+
+	system("pause");
+
+	return 0;
+}
