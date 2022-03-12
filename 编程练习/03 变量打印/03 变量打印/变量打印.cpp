ebc072d6dae41395f3e1375dd54ed892a96d0e13
影形 <2339254049@qq.com>
2022-03-12 15:23:40 +0800

    变量打印

---
+++ 编程练习/03 变量打印/03 变量打印/变量打印.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a = 0;
+
+	a = 10;
+
+	cout << "a=" << a << endl;
+
+	system("pause");
+
+	return 0;
+}
