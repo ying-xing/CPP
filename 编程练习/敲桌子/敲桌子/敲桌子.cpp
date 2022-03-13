77a72a70750c5a429b7d661e69bcb6cceca960b8
影形 <2339254049@qq.com>
2022-03-13 14:58:21 +0800

    敲桌子

---
+++ 编程练习/敲桌子/敲桌子/敲桌子.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{		
+		if (0 == i%7 || 7 == i%10 || 7 == i/10)
+		{
+			cout << "敲桌子" << endl;
+		}
+		else
+		{
+			cout << i << endl;
+		}
+	}
+
+
+
+	system("pause");
+	
+	return 0;
+}
